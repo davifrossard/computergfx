67716656f5853d40008b189e9a93839f8f542005
Davi Frossard <frossard@cs.toronto.edu>
2016-09-19 16:20:04 -0300

    Finished first version of Asg2

---
+++ Asg2/colours.h
@@ -0,0 +1,8 @@
+#include <GL/gl.h>
+#include <stdlib.h>
+#include <unordered_map>
+using namespace std;
+
+GLfloat* make_color(float r, float g, float b);
+
+unordered_map<string, GLfloat*> create_color_table();
